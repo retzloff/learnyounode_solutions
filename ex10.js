cbe8e5b4baa4ce85a3050534683d6e01fd18db79
Eric Retzloff <eretzloff@gmail.com>
2015-12-24 11:35:06 -0500

    Was not returning padded single digits. Fixed.

--- ex10.js
+++ ex10.js
@@ -3,18 +3,20 @@ var port = process.argv[2];
 
 var server = net.createServer( function(socket) {
     var date = new Date();
-    var year   = date.getFullYear();
-    var month  = date.getMonth()+1;
-    var day    = date.getDate();
-    var hour   = date.getHours();
-    var minute = date.getMinutes();
-    var timeStamp = [month, day, hour, minute];
+    var year   = pad(date.getFullYear())
+    var month  = pad(date.getMonth()+1)
+    var day    = pad(date.getDate())
+    var hour   = pad(date.getHours())
+    var minute = pad(date.getMinutes())
       
-    timeStamp.forEach( function(item) {
-        item = item + (item < 10 ? '0' : '') 
-    });
-        
-    socket.end([year, month, day].join('-') + ' ' + [hour, minute].join(':') + '\n');
+    socket.end([year, month, day].join('-') + ' ' + hour + ':' + minute + '\n');
 });
 
 server.listen(port);
+
+function pad(value) {
+    var result = value.toString()
+    if (result.length < 2)
+        result = '0' + result
+    return result
+}
