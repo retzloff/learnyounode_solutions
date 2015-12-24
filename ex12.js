641596d22c3ccd42244586563d518444dfa7f763
Eric Retzloff <eretzloff@gmail.com>
2015-12-23 23:03:29 -0500

    Exercise 12

---
+++ ex12.js
@@ -0,0 +1,15 @@
+var http = require('http')
+var map = require('through2-map')
+
+var server = http.createServer( function(req, res) {
+    if(req.method == 'POST') {
+        req.pipe(map(function(chunk){
+            return chunk.toString().toUpperCase()
+        })).pipe(res)        
+    } else {
+        return res.end('POSTs only please')
+    }
+    
+})
+
+server.listen(process.argv[2])
